d85464bad65343826207ab17f5cb5c1df0af574e
曾源喆 <adam880614@gmail.com>
2024-12-27 14:27:00 +0800

    Add

--- apps/crud/forms.py
+++ apps/crud/forms.py
@@ -33,18 +33,3 @@ class UserForm(FlaskForm):
     )
     #設定使用者表單中submit的內容
     submit = SubmitField("提交表單")
-    
-class CategoryForm(FlaskForm):
-    category = SelectField(
-        '選擇分類',
-        coerce=int,  # 表示選項的值是整數類型
-        choices=[],  # 稍後在視圖函數中動態填充
-        validators=[DataRequired(message="請選擇分類")]
-    )
-    new_category = StringField(
-        '新增分類',
-        validators=[
-            Length(max=50, message="分類名稱不能超過 50 個字符")
-        ]
-    )
-    submit = SubmitField('儲存分類')
